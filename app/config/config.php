e37862018302468a3fa1f5377b8c65baa34dbfdf
Diki Fauzi <akbareskhart@gmail.com>
2020-10-13 11:36:21 +0700

    Menambahkan DB phpMVC

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define ('BASEURL', 'http://localhost/rekweb2020_173040138_phpmvc/public');
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','rekweb2020_173040138_phpmvc');
--- app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define ('BASEURL', 'http://localhost/rekweb2020_173040138_phpmvc/public/');
